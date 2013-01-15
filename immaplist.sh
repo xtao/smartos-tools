50b2cb30c2033b7a2aa5d18e4a62aa01c80cce8f
XTao <xutao881001@gmail.com>
2013-01-15 17:25:48 +0800

    MOD: update

--- immaplist.sh
+++ immaplist.sh
@@ -5,6 +5,25 @@
 for image in `imgadm -p list`
 do
     attributes=( ${image//:/ } )
+    if [[ ${attributes[1]} == "smartos" ]] ; then
+        image_uuid=${attributes[0]}
+        ## `vmadm lookup image_uuid` doesn't work
+        i=0
+        machine_uuids=()
+        for uuid in `vmadm lookup type=OS`
+        do
+            u=$(vmadm get ${uuid} | json image_uuid)
+            if [[ "${u}" == "${image_uuid}" ]] ; then
+                machine_uuids[$i]=$uuid
+                ((i++))
+            fi
+        done
+        echo $image_uuid "smartos" ${#machine_uuids[*]}
+        for uuid in "${machine_uuids[@]}"
+        do
+            echo "\t"$uuid
+        done
+    fi
     if [[ ${attributes[1]} == "linux" ]] ; then
         image_uuid=${attributes[0]}
         machines=$(vmadm lookup disks.0.image_uuid=${image_uuid})
